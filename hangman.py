8feaa4a7eab983bf11e85970898781e95148a905
VictorEspinoza <victorej0311@gmail.com>
2021-03-19 17:13:01 -0700

    Hangman con entorno virtual shaw

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 from words import palabras
+from colorama import Fore, Style, init
 import random
-
+init()
 print("Bienvenido al ahorcado de insectos")
 
 def get_valid_word(palabras):
@@ -11,4 +12,4 @@ def get_valid_word(palabras):
 
     return palabra
 mi_palabra = get_valid_word(palabras)
-print(mi_palabra + '\n' , len(mi_palabra))
+print(mi_palabra +'\n', Fore.RED + '_ ' * len(mi_palabra))
